118f8540b630cf88c1a3fdd3c21b72b2a34e192c
JungwooSim <cscd053@gmail.com>
2024-05-16 17:09:57 +0900

    CRUD - WIP

--- quick-start-blog2/app/controllers/articles_controller.rb
+++ quick-start-blog2/app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
   def index
+    @article = "ALL"
+  end
+
+  # def show
+  #   @id = params[:id]
+  # end
+
+  def show
+    render json: { id: params[:id] }
   end
 end
